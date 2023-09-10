22e4e1eb1d1ab737b8dac77c4602bb8638d6a49f
Richard <richard.chrispe@hotmail.com>
2023-09-10 14:27:38 -0300

    Carrosel principal Pronto

---
+++ JavaScript/script.js
@@ -0,0 +1,15 @@
+let cont = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval(function(){
+    nextImage();
+}, 5000)
+
+function nextImage() {
+    cont++;
+    if (cont>6){
+        cont = 1;
+    }
+
+    document.getElementById("radio"+cont).checked = true;
+}
