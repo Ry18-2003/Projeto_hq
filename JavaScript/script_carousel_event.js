e2df9a2eb8619e6d6650c056cad09f2b433fc744
Richard <richard.chrispe@hotmail.com>
2023-09-16 00:57:02 -0300

    Colocando a casa em ordem

---
+++ JavaScript/script_carousel_event.js
@@ -0,0 +1,27 @@
+// Obtém referências para os elementos relevantes 1
+const opcao1 = document.getElementById("Ev1");
+const opcao2 = document.getElementById("Ev2");
+const opcao3 = document.getElementById("Ev3");
+const backColor1 = document.querySelector(".btn1");
+const backColor2 = document.querySelector(".btn2");
+const backColor3 = document.querySelector(".btn3");
+
+document.getElementById("Ev1").checked = true;
+backColor1.style.backgroundColor = "#000";
+
+// Adiciona um ouvinte de evento para cada opção de rádio
+opcao1.addEventListener("change", () => {
+    backColor1.style.backgroundColor = "#000";
+    backColor2.style.backgroundColor = "transparent";
+    backColor3.style.backgroundColor = "transparent";
+});
+opcao2.addEventListener("change", () => {
+    backColor2.style.backgroundColor = "#000";
+    backColor1.style.backgroundColor = "transparent";
+    backColor3.style.backgroundColor = "transparent";
+});
+opcao3.addEventListener("change", () => {
+    backColor3.style.backgroundColor = "#000";
+    backColor2.style.backgroundColor = "transparent";
+    backColor1.style.backgroundColor = "transparent";
+});
