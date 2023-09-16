7edd0f089b362506064b30d33c6664c7cc299d3d
Richard <richard.chrispe@hotmail.com>
2023-09-16 14:37:00 -0300

    Java script de carrosel supremo Pronto

--- JavaScript/script_carousel_sub.js
+++ JavaScript/script_carousel_sub.js
@@ -1,19 +1,39 @@
-const carousel = document.querySelector('.carousel');
-const prevButton = document.querySelector('.prev_button');
-const nextButton = document.querySelector('.next_button');
-let currentIndex = 0;
+// Função genérica para manipular a navegação do carrossel
+function handleCarouselNavigation(prevButton, nextButton, currentIndex, elementsPerPage, carouselClass) {
+    const prevButtonElement = document.querySelector(prevButton);
+    const nextButtonElement = document.querySelector(nextButton);
+    const carouselElement = document.querySelector(carouselClass);
 
-prevButton.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + carousel.children.length) % carousel.children.length;
-    updateCarousel();
-});
+    prevButtonElement.addEventListener('click', () => {
+        if (currentIndex > 0) {
+            currentIndex -= elementsPerPage;
+            updateCarousel(currentIndex, carouselElement);
+        } else {
+            currentIndex = elementsPerPage;
+            updateCarousel(currentIndex, carouselElement);
+        }
+    });
 
-nextButton.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % carousel.children.length;
-    updateCarousel();
-});
-
-function updateCarousel() {
-    const translateX = -currentIndex * 100 + '%';
-    carousel.style.transform = `translateX(${translateX})`;
+    nextButtonElement.addEventListener('click', () => {
+        if (currentIndex + elementsPerPage < carouselElement.children.length) {
+            currentIndex += elementsPerPage;
+            updateCarousel(currentIndex, carouselElement);
+        } else {
+            currentIndex = 0;
+            updateCarousel(currentIndex, carouselElement);
+        }
+    });
 }
+
+// Função para atualizar a exibição do carrossel
+function updateCarousel(currentIndex, carouselElement) {
+    const productWidth = 25; // Largura de cada elemento (produto) em porcentagem
+    const translateX = -currentIndex * productWidth + '%';
+    // Aplica a transformação CSS para mover o carrossel
+    carouselElement.style.transform = `translateX(${translateX})`;
+}
+
+// Exemplo de uso para os diferentes carrosséis
+handleCarouselNavigation('.prev1', '.next1', 0, 4, '.c1');
+handleCarouselNavigation('.prev2', '.next2', 0, 4, '.c2');
+handleCarouselNavigation('.prev3', '.next3', 0, 4, '.c3');
